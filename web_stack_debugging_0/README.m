43104b17b4d3dfa8fea2d906618951f3d7122cef
nshimyumurwa <marynshimyumurwa@gmail.com>
2025-11-19 18:08:28 +0000

    Add file

---
+++ web_stack_debugging_0/README.m
@@ -0,0 +1,5 @@
+Web Stack Debugging 
+
+This project focuses on debugging a web server inside a Docker container. The goal is to fix Apache so that querying the root of the server returns:
+
+Hello Holberton
